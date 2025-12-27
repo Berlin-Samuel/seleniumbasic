13b9d6b1f02872175152c7ed6e1bf61c57ac3345
Berlin-Samuel <rberlinsamuel@gmail.com>
2025-12-27 23:59:21 +0530

    completed wait methods

---
+++ src/test/java/com/mylearnings/waits/ImplicitWait.java
@@ -0,0 +1,52 @@
+/**
+ * FileName: ExplicitWait.java
+ * Author   : Admin
+ * Version  : 1.0
+ * Date     : 23-Dec-2025
+ * 
+ * This file is part of a personal learning project.
+ * 
+ * Licensed under the MIT License.
+ * 
+ * Copyright (c) 2025 Berlin Samuel.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy  
+ * of this software and associated documentation files (the "Software"), to deal  
+ * in the Software without restriction, including without limitation the rights  
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell  
+ * copies of the Software, and to permit persons to whom the Software is  
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in  
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * 
+ * REVISION     DATE           NAME         DESCRIPTION  
+ * 1.0          23-Dec-2025        Berlin        Initial Code  
+ * 
+ * @author Berlin
+ * @version 1.0
+ * @since 23-Dec-2025
+ */
+package com.mylearnings.waits;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWait {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@type=\"password\"]")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+
+	}
+
+}
