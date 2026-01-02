d807bcd109b5b5b61c6ceac7091d89f24a8d7e92
Berlin-Samuel <rberlinsamuel@gmail.com>
2026-01-02 14:26:19 +0530

    first commit

---
+++ src/test/java/com/mylearnings/handlingalerts/HandleAlertUsingExplicitWait.java
@@ -0,0 +1,55 @@
+/**
+ * FileName: Alert.java
+ * Author   : Admin
+ * Version  : 1.0
+ * Date     : 02-Jan-2026
+ * 
+ * This file is part of a personal learning project.
+ * 
+ * Licensed under the MIT License.
+ * 
+ * Copyright (c) 2026 Berlin Samuel.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy  
+ * of this software and associated documentation files (the "Software"), to deal  
+ * in the Software without restriction, including without limitation the rights  
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell  
+ * copies of the Software, and to permit persons to whom the Software is  
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in  
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * 
+ * REVISION     DATE           NAME         DESCRIPTION  
+ * 1.0          02-Jan-2026        Berlin        Initial Code  
+ * 
+ * @author Berlin
+ * @version 1.0
+ * @since 02-Jan-2026
+ */
+package com.mylearnings.handlingalerts;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HandleAlertUsingExplicitWait {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait myWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.navigate().to("https://the-internet.herokuapp.com/javascript_alerts");
+		driver.findElement(By.xpath("//button[@onclick='jsAlert()']")).click();
+		Alert myalert = myWait.until(ExpectedConditions.alertIsPresent());
+		myalert.accept();
+		driver.quit();
+	}
+}
