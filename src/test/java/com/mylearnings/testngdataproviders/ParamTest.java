2d869698ef390b605061e9dc6c304e55ffb1d4c3
Berlin-Samuel <rberlinsamuel@gmail.com>
2026-01-26 22:48:48 +0530

    parallel testing completed

--- src/test/java/com/mylearnings/testngdataproviders/ParamTest.java
+++ src/test/java/com/mylearnings/testngdataproviders/ParamTest.java
@@ -5,25 +5,42 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ParamTest {
 	WebDriver driver;
 
 	@BeforeClass
-	void setup() {
-		driver = new ChromeDriver();
+	@Parameters({ "browser" })
+	public void setup(String br) {
+		switch (br.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+		default:
+			System.out.println("Invalid Browser");
+			return;
+		}
 		driver.get("https://www.saucedemo.com/");
 		driver.manage().window().maximize();
 	}
 
 	@Test(priority = 1)
-	void testLogo() {
+	public void testLogo() {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		boolean status = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("login_logo")))
 				.isDisplayed();
@@ -31,19 +48,19 @@ public class ParamTest {
 	}
 
 	@Test(priority = 2)
-	void testTitle() {
+	public void testTitle() {
 		Assert.assertEquals(driver.getTitle(), "Swag Labs");
 	}
 
 	@Test(priority = 3)
-	void testURL() {
+	public void testURL() {
 		Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
 	}
 
 	@AfterClass
-	void tearDown() {
+	public void tearDown() {
 		if (driver != null) {
-			driver.quit();
+			driver.close();
 		}
 	}
 }
